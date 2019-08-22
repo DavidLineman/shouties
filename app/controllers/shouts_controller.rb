579a47f76560198d758d7f08e28f14626d85c266
DavidLineman <dline215@gmail.com>
2019-08-22 14:37:39 -0400

    added shouties form

---
+++ app/controllers/shouts_controller.rb
@@ -0,0 +1,22 @@
+class ShoutsController < ApplicationController
+
+  def create
+    shout = current_user.shouts.create(shout_params)
+    redirect_to root_path, redirect_options_for(shout)
+  end
+
+  private 
+
+  def shout_params
+    params.require(:shout).permit(:body)
+  end
+
+  def redirect_options_for(shout)
+    if shout.persisted?
+      { notice: "Shoutie Success...good on ya!"}
+    else 
+      { alert: "No Shout...silent scream!"}
+    end 
+  end
+
+end
