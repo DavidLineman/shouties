d45bfbcab734f60e71084ac2ecb3bb23dacf7f1c
DavidLineman <dline215@gmail.com>
2019-08-29 14:48:34 -0400

    added timeline class

---
+++ app/models/timeline.rb
@@ -0,0 +1,21 @@
+class Timeline
+
+  def initialize(users)
+    @users = users
+  end
+
+  def shouts
+    Shout.
+      where(user_id: @users).
+      order(created_at: :desc)
+  end
+
+  def to_partial_path
+    "timelines/timeline"
+  end
+
+  private
+
+  attr_reader :user
+
+end
